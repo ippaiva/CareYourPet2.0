4865de43457ff4f2a55fd3e35d2f8eea5b669aa2
Carol Park <carolpark09@gmail.com>
2018-12-03 18:15:53 -0200

    Created login routes

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
@@ -8,8 +8,10 @@ const userSchema = new Schema({
   timestamps: {
     createdAt: 'created_at',
     updatedAt: 'updated_at'
-  }
+  },
+  estabilishment: [{type: Schema.Types.ObjectId, ref: 'Estabilishment'}],
 });
 
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
-const router  = express.Router();
+
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+function isLogged(req) {
+  if (req.session.currentUser) {
+    return true;
+  }
+  return false;
+}
+
+router.get('/home', (req, res) => {
+  if (isLogged(req)) {
+    res.render('home');
+  } else {
+    res.render('index', {err: 'Please login.'});
+  }
+});
+
 
 module.exports = router;
