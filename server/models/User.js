cee3e24ff134d3e714a5966ae3a110989b178ec3
Ismael P. Paiva <ismael.paiva@fatec.sp.gov.br>
2018-12-06 14:14:58 -0200

    ajust in server

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  pictureUrl: String,
   username: String,
   password: String,
   name: String,
