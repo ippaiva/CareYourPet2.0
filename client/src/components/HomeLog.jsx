5d6c16552b80735c44c1ccd2f324e0ee0586624f
Carol Park <carolpark09@gmail.com>
2018-12-07 19:01:25 -0200

    Added Card to Home

--- client/src/components/HomeLog.jsx
+++ client/src/components/HomeLog.jsx
@@ -13,27 +13,35 @@ class HomeLog extends Component {
   render() {
     return (
       <div className="homeLog">
+
         <div className="sidebar">
           <Navbar />
         </div>
+
         <div>
           <img src="/img/mountains.jpg" alt="loggedImage" height=""></img>
           <input className="input" type="text" placeholder="Search"></input>
         </div>
+        
         <div>
-        {/* <div className="hero">
-          <div className="card">
-            <figure className="image is-4by3">
-              <img src="img/home1.jpg" alt  ="Animals">
-              </img>
-            </figure>
-          </div>
-          <div className="card-content">
-            <div className="content">
-              Be part of this growing community!
+
+          {/* Card Component */}
+          <div className="hero">
+            <div className="card">
+              <figure className="image is-4by3">
+                <img src="img/retriever.jpg" alt  ="Animals">
+                </img>
+              </figure>
+            </div>
+            <div className="card-content">
+              <div className="content">
+                <h4>Avengers Petshop</h4>
+                <p>Grooming and Vet</p>
+              </div>
             </div>
           </div>
-        </div> */}
+
+
       </div>
       </div>
     )
