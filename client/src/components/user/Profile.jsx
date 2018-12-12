6cc0b9fb16d954a64a0a0fb57edee1c973f8ca3b
Carol Park <carolpark09@gmail.com>
2018-12-12 17:00:55 -0200

    Profile style

--- client/src/components/user/Profile.jsx
+++ client/src/components/user/Profile.jsx
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from '../navbar/Navbar';
 
-const Profile = (props) => {
-  return (
-    <div className="sidebarContainer">
-      <div className="sidebar">
-        <Navbar />
+class Profile extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      name: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      zipcode: '',
+      address: ''
+    };
+  }
 
-      </div>
-
-      <div className="formContainer">
-        <form className="form" method="get">
-          <h1 className="title is-1" >My Profile</h1>
-          Username: <input type="text" name="Username" value="" /><br></br>
-          Name: <input type="text" name="Name" value="" /><br></br>
-          Last Name: <input type="text" name="LastName" value="" /><br></br>
-          Email: <input type="text" name="Email" value="" /><br></br>
-          Phone: <input type="text" name="Phone" value="" /><br></br>
-          Zipcode: <input type="text" name="Zipcode" value="" /><br></br>
-          Address: <input type="text" name="Address" value="" /><br></br>
-        </form>
-
-      </div>
-
-    </div>
-  )
+  handleChange = (event) => {
+    const {name, value} = event.target;
+    console.log(value);
+    this.setState({[name]: value});
+    console.log(this.state)
 }
+  render() {
+    return (
+      <div className="sidebarContainerProfile">
+        <div className="sidebar">
+          <Navbar />
+    
+        </div>
+    
+        <div className="formContainerOutter">
+          <div className="formContainer">
+            <form className="form" method="get">
+              <h1 className="title is-1" >My Profile</h1>
+              Username: <input type="text" name="Username" value={this.state.username} onChange={ e => this.handleChange(e)} /><br></br>
+              Name: <input type="text" name="Name" value={this.state.name} onChange={ e => this.handleChange(e)} /><br></br>
+              Last Name: <input type="text" name="LastName" value={this.state.lastName} onChange={ e => this.handleChange(e)} /><br></br>
+              Email: <input type="text" name="Email" value={this.state.email} onChange={ e => this.handleChange(e)} /><br></br>
+              Phone: <input type="text" name="Phone" value={this.state.phone} onChange={ e => this.handleChange(e)} /><br></br>
+              Zipcode: <input type="text" name="Zipcode" value={this.state.zipcode} onChange={ e => this.handleChange(e)} /><br></br>
+              Address: <input type="text" name="Address" value={this.state.address} onChange={ e => this.handleChange(e)} /><br></br>
+            </form>
+            
+          </div>
+    
+        </div>
+    
+      </div>
+    )
+  }
+};
 
 export default Profile;
