fba37a90c3acce6cfc46d3cc9fe3e4b6563acbb2
Carol Park <carolpark09@gmail.com>
2018-12-13 19:05:15 -0200

    Resolved conflicts

--- client/src/components/establishments/AddEstablishment.jsx
+++ client/src/components/establishments/AddEstablishment.jsx
@@ -3,20 +3,6 @@ import axios from "axios";
 import Navbar from "../navbar/Navbar";
 
 class AddEstablishment extends Component {
-<<<<<<< HEAD
-  constructor(props){
-      super(props);
-      this.state = { 
-        establishmentName: "",
-        address: "",
-        zipCode:"",
-        phone:"",
-        type:"",
-        services:"",
-        workingHours:"",
-        site:""
-      };
-=======
   constructor(props) {
     super(props);
     this.state = {
@@ -29,7 +15,6 @@ class AddEstablishment extends Component {
       workingHours: [],
       site: ""
     };
->>>>>>> 6b605dd94935776285561e4c78fad5f7874b79e6
   }
 
   handleFormSubmit = event => {
